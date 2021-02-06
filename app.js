dfd0a182f3d23aa94a57af8bdb8812b66bfdafbb
Nahid <nahidnahid63@gmail.com>
2021-02-07 04:17:48 +0600

    half

--- app.js
+++ app.js
@@ -7,44 +7,38 @@ fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${dishName}`)
 const displayFood = meals=>{
     const foodsDiv= document.getElementById('foods');
     const menu=meals.meals;
+    foodsDiv.innerHTML="";
     menu.forEach(meal => {
         const foodDiv= document.createElement('div');
         foodDiv.className='food'
         const foodInfo=`
+        <div onclick="displayFoodDetail('${meal.strMeal}')">
             <h3>${meal.strMeal}</h>
-            <img src="${meal.strMealThumb}"
+            <img id="firstImg" src="${meal.strMealThumb}">
+        </div>    
+
         `;
         foodDiv.innerHTML= foodInfo;
         foodsDiv.appendChild(foodDiv);
     });
-    // for(let i=0;i<menu.length;i++)
-    // {
-    //     const meal =menu[i];
-    //     const foodDiv= document.createElement('div');
-    //     // const h3 =document.createElement('h3');
-    //     // h3.innerText=meal.strMeal;
-    //     // foodDiv.appendChild(h3);
-    //     // const img =document.createElement('img');
-    //     // img.innerText=meal.strMealThumb;
-    //     // foodDiv.appendChild(img);
-    //     foodDiv.className='food'
-    //     const foodInfo=`
-    //         <h3>${meal.strMeal}</h>
-    //         <img src="${meal.strMealThumb}"
-    //     `;
-    //     foodDiv.innerHTML= foodInfo;
-    //     foodsDiv.appendChild(foodDiv);
-    // }
 }
-//     const foodsDiv =document.getElementById('foods');
-//     countries.forEach(meal => {
-//         const foodDiv=document.createElement('div');
 
-//         foodDiv.className='country';
-//         const foodInfo =`
-//             <h3 class="food-name">${food.name}</h3> `
-//         foodDiv.innerHTML =foodInfo;      
-//         foodsDiv.appendChild(foodDiv);
-//     });
-// }
 }
+const displayFoodDetail=strMeal=>{
+    const url =`https://www.themealdb.com/api/json/v1/1/search.php?s=${strMeal}`
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>console.log(data));
+}
+// const randerFoodInfo=meals=>{
+//     const oneMeal=meals.meals;
+//     const onemenu=oneMeal[0];
+//     const foodDiv=document.getElementsByClassName('ingradience');
+//     foodDiv.innerHTML=`
+//         <img src="${onemenu.strMealThumb}">
+//         <h3>${onemenu.strMeal}</h3>
+//         <li>${onemenu.strIngredient1}</li>   
+//     `;
+
+
+// }
