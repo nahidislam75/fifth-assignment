861ee10a8be752c045cf229d72b33a061a310fdb
Nahid <nahidnahid63@gmail.com>
2021-02-07 21:55:11 +0600

    alldonebro

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-function lookForDish(){
-const dishName =document.getElementById('dish-catagory').value;
-fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${dishName}`)
-.then(res=>res.json())
-.then(data=>displayFood(data))
 
-const displayFood = meals=>{
-    const foodsDiv= document.getElementById('foods');
-    const menu=meals.meals;
-    foodsDiv.innerHTML="";
-    menu.forEach(meal => {
-        const foodDiv= document.createElement('div');
-        foodDiv.className='food'
-        const foodInfo=`
+function lookForDish() {
+    const dishName = document.getElementById('dish-catagory').value;
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${dishName}`)
+        .then(res => res.json())
+        .then(data => displayFood(data))
+        .catch(error=> window.alert('Can not find your food'))
+    const displayFood = meals => {
+        const foodsDiv = document.getElementById('foods');
+        const menu = meals.meals;
+        foodsDiv.innerHTML = "";
+        menu.forEach(meal => {
+            const foodDiv = document.createElement('div');
+            foodDiv.className = 'food'
+            const foodInfo = `
         <div onclick="displayFoodsDetails('${meal.strMeal}')">
         <img id="firstImg" src="${meal.strMealThumb}">
             <h3>${meal.strMeal}</h>
@@ -19,35 +20,40 @@ const displayFood = meals=>{
         </div>    
 
         `;
-        foodDiv.innerHTML= foodInfo;
-        foodsDiv.appendChild(foodDiv);
-    });
+            foodDiv.innerHTML = foodInfo;
+            foodsDiv.appendChild(foodDiv);
+        });
+    }
+
 }
-}
-const displayFoodsDetails=strMeal=>{
-    const url =`https://www.themealdb.com/api/json/v1/1/search.php?s=${strMeal}`
+
+const displayFoodsDetails = strMeal => {
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${strMeal}`
     fetch(url)
-    .then(res=>res.json())
-    .then(data=>displayFoodsDetail(data));
+        .then(res => res.json())
+        .then(data => displayFoodsDetail(data));
 }
-const displayFoodsDetail = meals=>{
-    const foodsDiv= document.getElementById('ingradience');
-    const menu=meals.meals;
-    foodsDiv.innerHTML="";
+const displayFoodsDetail = meals => {
+    const foodsDiv = document.getElementById('ingradience');
+    const menu = meals.meals;
+    foodsDiv.innerHTML = "";
     menu.forEach(meal => {
-        const foodDiv= document.createElement('div');
-        foodDiv.className='food'
-        const foodInfo=`
+        const foodDiv = document.createElement('div');
+        foodDiv.className = 'food'
+        const foodInfo = `
         <img src="${meal.strMealThumb}">
          <h3>${meal.strMeal}</h3>
+         <p>Ingredints</p>
+         <ul class="list-item">
          <li>${meal.strIngredient1}</li> 
          <li>${meal.strIngredient2}</li> 
          <li>${meal.strIngredient3}</li> 
          <li>${meal.strIngredient4}</li> 
          <li>${meal.strIngredient5}</li> 
-
+         <li>${meal.strIngredient6}</li> 
+        </ul>
         `;
-        foodDiv.innerHTML= foodInfo;
+        foodDiv.innerHTML = foodInfo;
         foodsDiv.appendChild(foodDiv);
     });
 }
